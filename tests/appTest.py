361315a00c4ad47ccecbcb8d2ce692f62b539491
Beatriz Rosa <142547489+beatrizrosaa@users.noreply.github.com>
2026-03-25 22:35:52 +0000

    finalizou com 100% de apuragem

--- tests/appTest.py
+++ tests/appTest.py
@@ -10,3 +10,15 @@ class AppTest(unittest.TestCase):
         self.assertEqual(200, response.status_code, "Erro no test_http_code!")
         self.assertEqual("<h1>Hello, I'm Alive!</h1>", response.get_data(as_text=True)
                           , "Erro no test_print_health_check!")
+
+    def test_hello_no_name(self):
+        # Testa a rota /hello sem passar o parâmetro 'name' (deve retornar 400)
+        response = self.app.get('/hello')
+        self.assertEqual(400, response.status_code)
+        self.assertEqual("Nome não informado", response.get_data(as_text=True))
+
+    def test_hello_with_name(self):
+        # Testa a rota /hello passando o parâmetro 'name'
+        response = self.app.get('/hello?name=Beatriz')
+        self.assertEqual(200, response.status_code)
+        self.assertEqual("Hello, Beatriz!", response.get_data(as_text=True))
